8a8a11439010e335e7767899457e92a50989f16f
กวิน รัตนพันธ์ <43963760+athensclub@users.noreply.github.com>
2024-12-10 23:05:41 +0700

    full inference

--- analysis_move/main.py
+++ analysis_move/main.py
@@ -1,58 +1,282 @@
-import chess
+from unittest import TestCase
+from itertools import chain
+import re
 
-def analysis_move(fen_positions):
-    # Initialize the board
+import chess
+import chess.pgn
+
+
+def parse(fen_str):
+    ranks = fen_str.split(" ")[0].split("/")
+    pieces_on_all_ranks = [parse_rank(rank) for rank in ranks]
+    return pieces_on_all_ranks
+
+def parse_rank(rank):
+    rank_re = re.compile("(\d|[kqbnrpKQBNRP])")
+    piece_tokens = rank_re.findall(rank)
+    pieces = flatten(map(expand_or_noop, piece_tokens))
+    return pieces
+
+def flatten(lst):
+    return list(chain(*lst))
+
+def expand_or_noop(piece_str):
+    piece_re = re.compile("([kqbnrpKQBNRP])")
+    retval = ""
+    if piece_re.match(piece_str):
+        retval = piece_str
+    else:
+        retval = expand(piece_str)
+    return retval
+
+def expand(num_str):
+    return int(num_str)*" "
+
+
+def test_parse_rank():
+    start_pos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    rank8 = "rnbqkbnr"
+    rank7 = "pppppppp"
+    rank6 = "8"
+    fp = parse(start_pos)
+    assert fp.parse_rank(rank8) == ["r","n","b","q","k","b","n","r"]
+    assert fp.parse_rank(rank7) == ["p","p","p","p","p","p","p","p"]
+    assert fp.parse_rank(rank6) == [" "," "," "," "," "," "," "," "]
+
+def test_parse_starting_position(pos):
+    start_pos = pos
+    fp = parse(start_pos)
+    return fp
+
+cols = ["a", "b", "c", "d", "e", "f", "g", "h"]
+rows = ["8", "7", "6", "5", "4", "3", "2", "1"]
+
+def get_state(a, b, c):
+    if a == "p" or a == 'P':
+        a = ""
+    return a.upper() + cols[c] + rows[b]
+
+def check_specific_case(frame_a, frame_b, point_a, point_b):
+    character = frame_b[point_b[0]][point_b[1]]
+
+    dx = []
+    dy = []
+    if character == "N" or character == "n":
+        dx = [-2, -1, 1, 2, -2, -1, 1, 2]
+        dy = [-1 ,-2, -2, -1, 1, 2, 2, 1]
+        
+        cnt = 0
+        for i in range(len(dx)):
+            if point_b[0] + dx[i] < 0 or point_b[0] + dx[i] > 7 or point_b[1] + dy[i] < 0 or point_b[1] + dy[i] > 7:
+                continue
+
+            point_c = [point_b[0] + dx[i], point_b[1] + dy[i]]
+            if frame_a[point_c[0]][point_c[1]] == character:
+                cnt += 1
+
+        if cnt == 2:
+            return cols[point_a[1]]
+        return ""
+
+    elif character == "R" or character == 'r':
+        dx = [
+            [-1, -2, -3, -4, -5, -6, -7],
+            [1, 2, 3, 4, 5, 6, 7]
+        ]
+        dy = [
+            [0, 0, 0, 0, 0, 0, 0], 
+            [0, 0, 0, 0, 0, 0, 0]
+        ]
+
+        cnt = 0
+        for i in range(len(dx)):
+            for j in range(len(dx[0])):
+                if point_b[0] + dx[i][j] < 0 or point_b[0] + dx[i][j] > 7 or point_b[1] + dy[i][j] < 0 or point_b[1] + dy[i][j] > 7:
+                    continue
+
+                point_c = [point_b[0] + dx[i][j], point_b[1] + dy[i][j]]
+                if frame_a[point_c[0]][point_c[1]] == character:
+                    cnt += 1
+                    break
+        if cnt == 2:
+            return rows[point_a[0]]
+                
+        dx = [
+            [0, 0, 0, 0, 0, 0, 0], 
+            [0, 0, 0, 0, 0, 0, 0]
+        ]
+        dy = [
+            [-1, -2, -3, -4, -5, -6, -7],
+            [1, 2, 3, 4, 5, 6, 7]
+        ]
+
+        cnt = 0
+        for i in range(len(dx)):
+            for j in range(len(dx[0])):
+                if point_b[0] + dx[i][j] < 0 or point_b[0] + dx[i][j] > 7 or point_b[1] + dy[i][j] < 0 or point_b[1] + dy[i][j] > 7:
+                    continue
+
+                point_c = [point_b[0] + dx[i][j], point_b[1] + dy[i][j]]
+                if frame_a[point_c[0]][point_c[1]] == character:
+                    cnt += 1
+                    break
+        if cnt == 2:
+            return cols[point_a[1]]
+        
+        dx = [
+            [0, 0, 0, 0, 0, 0, 0], 
+            [0, 0, 0, 0, 0, 0, 0],
+            [-1, -2, -3, -4, -5, -6, -7],
+            [1, 2, 3, 4, 5, 6, 7]
+        ]
+        dy = [
+            [-1, -2, -3, -4, -5, -6, -7],
+            [1, 2, 3, 4, 5, 6, 7],
+            [0, 0, 0, 0, 0, 0, 0], 
+            [0, 0, 0, 0, 0, 0, 0],
+        ]
+
+        cnt = 0
+        for i in range(len(dx)):
+            for j in range(len(dx[0])):
+                if point_b[0] + dx[i][j] < 0 or point_b[0] + dx[i][j] > 7 or point_b[1] + dy[i][j] < 0 or point_b[1] + dy[i][j] > 7:
+                    continue
+
+                point_c = [point_b[0] + dx[i][j], point_b[1] + dy[i][j]]
+                if frame_a[point_c[0]][point_c[1]] == character:
+                    cnt += 1
+                    break
+        if cnt == 2:
+            return cols[point_a[1]]
+    return ""
+
+def get_move(frame_a, frame_b, point_a, point_b):
+    move = get_state(frame_b[point_b[0]][point_b[1]] ,point_b[0], point_b[1])
+        
+    if frame_a[point_b[0]][point_b[1]] != ' ':
+        if frame_a[point_a[0]][point_a[1]] == "p" or frame_a[point_a[0]][point_a[1]] == "P":
+            move = cols[point_a[1]] + "x" + move
+            if move[0] == move[2]:
+                move = move[:2] + move[3:]
+        else:
+            move = frame_a[point_a[0]][point_a[1]].upper() + "x" + move
+            if move[0] == move[2]:
+                move = move[:2] + move[3:]
+
+    move = move[:1] + check_specific_case(frame_a, frame_b, [point_a[0], point_a[1]], [point_b[0], point_b[1]]) + move[1:]
+    return move
+
+def replace_king(frame, charector):
+    temp = [[], [], []]
+    for i in range(0, len(frame)):
+        for j in range(0, len(frame[0])):
+            if frame[i][j] == charector[0]:
+                temp[0].append([i, j])
+            elif frame[i][j] == charector[1]:
+                temp[1].append([i, j])
+            elif frame[i][j] == charector[2]:
+                temp[2].append([i, j])
+    
+    if len(temp[0]) == 0:
+        if len(temp[1]) > 0:
+            frame[temp[1][0][0]][temp[1][0][1]] = charector[0]
+        elif len(temp[2]) > 0:
+            frame[temp[2][0][0]][temp[2][0][1]] = charector[0]
+
+def some_shit(frame):
+    replace_king(frame, ['k', 'q', 'r'])
+    replace_king(frame, ['K', 'Q', 'R'])
+        
+def get_pgn(frame_a, frame_b):
+    frame_a = test_parse_starting_position(frame_a)
+    frame_b = test_parse_starting_position(frame_b)
+    some_shit(frame_a)
+    some_shit(frame_b)
+    points = list()
+
+    for i in range(0, len(frame_a)):
+        for j in range(0, len(frame_b)):
+            if frame_a[i][j] != frame_b[i][j]:
+                points.append([i, j])
+
+    if len(points) == 0 or len(points) > 2: return "not_change"
+
+    if frame_b[points[0][0]][points[0][1]] == ' ':
+        move = get_move(frame_a, frame_b, points[0], points[1])
+    else:
+        move = get_move(frame_a, frame_b, points[1], points[0])
+    return move
+
+def check_mate(pgn_input):
+    # Input PGN without annotations
+    # pgn_input = "1. e4 e5 2. Ba6 bxa6 3. Nc3 d5 4. Nxd5 Qxd5 5. exd5 Kd7 6. Qg4 Kd6 7. Qxc8 Nd7 8. Qxa8 Be7 9. Qc6"
+
+    # Parse moves
+    moves = pgn_input.replace(".", "").split()
+
+    # Initialize a chess board
     board = chess.Board()
 
-    # List to store moves in SAN format
-    san_moves = []
+    # Annotate moves
+    annotated_moves = []
+    for move in moves:
+        try:
+            chess_move = board.parse_san(move)
+            board.push(chess_move)
+            if board.is_checkmate():
+                annotated_moves.append(f"{move}#")
+            elif board.is_check():
+                annotated_moves.append(f"{move}+")
+            else:
+                annotated_moves.append(move)
+        except ValueError:
+            annotated_moves.append(move)  # In case of invalid move, keep as-is
 
-    # Iterate through FEN positions
-    for i in range(len(fen_positions) - 1):
-        board.set_fen(fen_positions[i])  # Set the current board position
-        next_board = chess.Board(fen_positions[i + 1])  # Set the next board position
-        
-        # Find the move that leads to the next position
-        correct_move = None
-        for move in board.legal_moves:
-            board.push(move)
-            if board.fen() == fen_positions[i + 1]:
-                correct_move = move
-                board.pop()  # Undo the move
-                break
-            board.pop()  # Undo the move if not matching
-        
-        if correct_move:
-            san_moves.append(board.san(correct_move))  # Append the correct move in SAN format
-            board.push(correct_move)  # Apply the correct move to the board
+    # Reconstruct the PGN with annotations
+    annotated_pgn = " ".join(annotated_moves)
 
-    # Join moves into a single string
-    result_string = " ".join(san_moves)
-    # print(result_string)
-    return result_string
+    # Output
+    # print(annotated_pgn)
+    return annotated_pgn
 
-def main(fen_positions):
-    return analysis_move(fen_positions)
+def transform_chess_notation(input_moves):
+    # Split the input into individual moves
+    moves = input_moves.split(" ")
+    
+    # Filter out any existing numbering
+    moves = [move for move in moves if not move.isdigit()]
+    
+    # Initialize variables for the output string and move counter
+    formatted_moves = ""
+    move_counter = 1
+    
+    # Loop through the moves, adding the move counter for every two moves
+    for i in range(0, len(moves), 2):
+        if i + 1 < len(moves):
+            formatted_moves += f"{move_counter}. {moves[i]} {moves[i+1]} "
+        else:
+            formatted_moves += f"{move_counter}. {moves[i]} "
+        move_counter += 1
+    
+    # Remove the trailing space and return the result
+    return formatted_moves.strip()
 
-# test_case
-test_case = [
-    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-    "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1",
-    "rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2",
-    "rnbqkbnr/pppp1ppp/B7/4p3/4P3/8/PPPP1PPP/RNBQK1NR b KQkq - 1 2",
-    "rnbqkbnr/p1pp1ppp/p7/4p3/4P3/8/PPPP1PPP/RNBQK1NR w KQkq - 0 3",
-    "rnbqkbnr/p1pp1ppp/p7/4p3/4P3/2N5/PPPP1PPP/R1BQK1NR b KQkq - 1 3",
-    "rnbqkbnr/p1p2ppp/p7/3pp3/4P3/2N5/PPPP1PPP/R1BQK1NR w KQkq - 0 4",
-    "rnbqkbnr/p1p2ppp/p7/3Np3/4P3/8/PPPP1PPP/R1BQK1NR b KQkq - 0 4",
-    "rnb1kbnr/p1p2ppp/p7/3qp3/4P3/8/PPPP1PPP/R1BQK1NR w KQkq - 0 5",
-    "rnb1kbnr/p1p2ppp/p7/3Pp3/8/8/PPPP1PPP/R1BQK1NR b KQkq - 0 5",
-    "rnb2bnr/p1pk1ppp/p7/3Pp3/8/8/PPPP1PPP/R1BQK1NR w KQ - 1 6",
-    "rnb2bnr/p1pk1ppp/p7/3Pp3/6Q1/8/PPPP1PPP/R1B1K1NR b KQ - 2 6",
-    "rnb2bnr/p1p2ppp/p2k4/3Pp3/6Q1/8/PPPP1PPP/R1B1K1NR w KQ - 3 7",
-    "rnQ2bnr/p1p2ppp/p2k4/3Pp3/8/8/PPPP1PPP/R1B1K1NR b KQ - 0 7",
-    "r1Q2bnr/p1pn1ppp/p2k4/3Pp3/8/8/PPPP1PPP/R1B1K1NR w KQ - 1 8",
-    "Q4bnr/p1pn1ppp/p2k4/3Pp3/8/8/PPPP1PPP/R1B1K1NR b KQ - 0 8",
-    "Q5nr/p1pnbppp/p2k4/3Pp3/8/8/PPPP1PPP/R1B1K1NR w KQ - 1 9",
-    "6nr/p1pnbppp/p1Qk4/3Pp3/8/8/PPPP1PPP/R1B1K1NR b KQ - 2 9"
-]
-
+def main(frame):
+    state = "white"
+    num = 1
+    ans = ""
+    for i in range(1, len(frame)):
+        state_change = get_pgn(frame[i-1], frame[i])
+        if state_change == "not_change":
+            continue
+        if state == "white":
+            state = "black"
+            a = state_change
+            ans += str(num) + ". " + a + " "
+        elif state == "black":
+            state = "white"
+            b = state_change
+            ans += b + " "
+            num += 1
+    ans = check_mate(ans)
+    return transform_chess_notation(ans)
--- kangdb/main.py
+++ kangdb/main.py
@@ -1,6 +1,7 @@
 import chess
 import cv2
-from analysis_move.main import analysis_move
+import os
+from analysis_move.main import main as analysis_move
 from extract_video.main import extract_unique_frames
 from chesscog.recognition.recognition import ChessRecognizer
 from recap import URI
@@ -18,15 +19,30 @@ def extract_moves(video_path):
     fens = [predict_state(frame, idx) for idx, frame in enumerate(frames)]
     for idx, fen in enumerate(fens):
         print( f"Frame {idx+1}: https://lichess.org/editor/{fen}")
-    print('moves',analysis_move(fens))
+    moves = analysis_move(fens)
+    print('moves', moves)
+    return moves
 
 
 if __name__ == "__main__":
     # read video path from args
-    import argparse
-    parser = argparse.ArgumentParser(description="Extract unique frames from a video.")
-    parser.add_argument("video_path", type=str, help="Path to the input video file.")
-    args = parser.parse_args()
+    # import argparse
+    # parser = argparse.ArgumentParser(description="Extract unique frames from a video.")
+    # parser.add_argument("video_path", type=str, help="Path to the input video file.")
+    # args = parser.parse_args()
 
-    video_path = args.video_path
-    extract_moves(video_path)
+    # video_path = args.video_path
+    root = "C:/Users/Athens/Downloads/cu-chess-detection/test_videos"
+    
+    res = [['row_id', "output"]]
+    for filename in os.listdir(root):
+        video_path = os.path.join(root, filename)
+        moves = extract_moves(video_path)
+        res.append([filename, moves])
+        
+    # write to csv
+    import csv
+    with open('C:/Users/Athens/Downloads/output.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(res)
+    
